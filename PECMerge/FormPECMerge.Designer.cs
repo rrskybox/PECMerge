7c232d7ee995a64f59c4542c4b09a57ba8dffe78
Richard McAlister <rick-mcalister@msn.com>
2020-03-07 06:44:35 -0800

    -Turned the average frequency plot to a spline. - Changed the color for individual PEC curves to green. - Housekeeping

--- PECMerge/FormPECMerge.Designer.cs
+++ PECMerge/FormPECMerge.Designer.cs
@@ -28,15 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.LoadFilesButton = new System.Windows.Forms.Button();
             this.PECResultsFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.PECchart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.PECCurve = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.noiseTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.PECchart)).BeginInit();
+            this.PECFrequency = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BestLogBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PECCurve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PECFrequency)).BeginInit();
             this.SuspendLayout();
             // 
             // LoadFilesButton
@@ -53,53 +61,98 @@
             // 
             this.PECResultsFileDialog.Multiselect = true;
             // 
-            // PECchart
+            // PECCurve
             // 
-            chartArea2.Name = "ChartArea1";
-            this.PECchart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.PECchart.Legends.Add(legend2);
-            this.PECchart.Location = new System.Drawing.Point(12, 56);
-            this.PECchart.Name = "PECchart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.PECchart.Series.Add(series2);
-            this.PECchart.Size = new System.Drawing.Size(776, 417);
-            this.PECchart.TabIndex = 1;
-            this.PECchart.Text = "chart1";
+            chartArea1.Name = "ChartArea1";
+            this.PECCurve.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.PECCurve.Legends.Add(legend1);
+            this.PECCurve.Location = new System.Drawing.Point(12, 56);
+            this.PECCurve.Name = "PECCurve";
+            this.PECCurve.Size = new System.Drawing.Size(776, 230);
+            this.PECCurve.TabIndex = 1;
+            this.PECCurve.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "PE Correction at Index";
+            this.PECCurve.Titles.Add(title1);
             // 
             // noiseTextBox
             // 
-            this.noiseTextBox.Location = new System.Drawing.Point(545, 22);
+            this.noiseTextBox.Location = new System.Drawing.Point(444, 18);
             this.noiseTextBox.Name = "noiseTextBox";
-            this.noiseTextBox.Size = new System.Drawing.Size(100, 20);
+            this.noiseTextBox.Size = new System.Drawing.Size(53, 20);
             this.noiseTextBox.TabIndex = 2;
+            this.noiseTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(413, 25);
+            this.label1.Location = new System.Drawing.Point(312, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Average Estimated Noise";
+            // 
+            // PECFrequency
+            // 
+            chartArea2.AxisX.IsStartedFromZero = false;
+            chartArea2.Name = "ChartArea1";
+            this.PECFrequency.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.PECFrequency.Legends.Add(legend2);
+            this.PECFrequency.Location = new System.Drawing.Point(12, 292);
+            this.PECFrequency.Name = "PECFrequency";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.PECFrequency.Series.Add(series1);
+            this.PECFrequency.Size = new System.Drawing.Size(776, 230);
+            this.PECFrequency.TabIndex = 4;
+            this.PECFrequency.Text = "chart1";
+            title2.Name = "Title1";
+            title2.Text = "Correction Frequency (per worm turn)";
+            this.PECFrequency.Titles.Add(title2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(513, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Best Fit";
+            // 
+            // BestLogBox
+            // 
+            this.BestLogBox.Location = new System.Drawing.Point(561, 18);
+            this.BestLogBox.Name = "BestLogBox";
+            this.BestLogBox.Size = new System.Drawing.Size(227, 20);
+            this.BestLogBox.TabIndex = 5;
+            this.BestLogBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FormPECMerge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BestLogBox);
+            this.Controls.Add(this.PECFrequency);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.noiseTextBox);
-            this.Controls.Add(this.PECchart);
+            this.Controls.Add(this.PECCurve);
             this.Controls.Add(this.LoadFilesButton);
             this.Name = "FormPECMerge";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.PECchart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PECCurve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PECFrequency)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +162,12 @@
 
         private System.Windows.Forms.Button LoadFilesButton;
         private System.Windows.Forms.OpenFileDialog PECResultsFileDialog;
-        private System.Windows.Forms.DataVisualization.Charting.Chart PECchart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PECCurve;
         private System.Windows.Forms.TextBox noiseTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PECFrequency;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BestLogBox;
     }
 }
 
--- PECMerge/FormPECMerge.cs
+++ PECMerge/FormPECMerge.cs
@@ -1,138 +1,329 @@
 ﻿using System;
-using System.Drawing;
-using System.Windows.Forms;
-using System.IO;
-using System.Windows.Forms.DataVisualization.Charting;
 using System.Deployment.Application;
+using System.Drawing;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PECMerge
 {
+
     public partial class FormPECMerge : Form
     {
+        public string[] pecfiles = null;
+        public int dpntCount = 0;
+        public int fileCount = 0;
+        public double[,] dpntArray = null;
+        public double[] avgVals;  //average per even sample
+        public double[] noiseVals;  //average per odd sample
+        public string clipOut;
+
         public FormPECMerge()
         {
             InitializeComponent();
             // Acquire the version information and put it in the form header
             try { this.Text = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString(); }
             catch { this.Text = " in Debug"; } //probably in debug, no version info available
-            this.Text = "PECMerge V" + this.Text;
+            this.Text = "PEC Merge V" + this.Text;
         }
 
         private void LoadFilesButton_Click(object sender, EventArgs e)
         {
-            double[,] idxVal = null;
-            int idxCount = 0;
-            char[] spc = { ' ', '\t' };
-
             PECResultsFileDialog.ShowDialog();
-            string[] pecfiles = PECResultsFileDialog.FileNames;
-            int fileCount = pecfiles.Length;
+            pecfiles = PECResultsFileDialog.FileNames;
+
+            GraphFiles();
+            return;
+        }
+
+        public void GraphFiles()
+        {
+            Color logCurveColor = Color.Green;
+            Color avgCurveColor = Color.Blue;
+            Color logFreqColor = Color.Blue;
+            Color noiseColor = Color.Red;
+            Color medianFreqColor = Color.Red;
+
+            int logCurveBorderWidth = 1;
+            int avgCurveBorderWidth = 1;
+            int logFreqBorderWidth = 1;
+            int medianFreqBorderWidth = 1;
+            int noiseBorderWidth = 1;
+
+
+            const int maxPeriod = 20;
+            fileCount = pecfiles.Length;
+            dpntCount = 0;
+            clipOut = null;
+
+            Complex[,] cplxArray = null;
+            Complex[,] fpntArray = null;
+
+
+            char[] spc = { ' ', '\t' };
 
             for (int f = 0; f < fileCount; f++)
             {
                 string[] fStringVals = File.ReadAllLines(pecfiles[f]);
-                idxCount = fStringVals.Length;
+                dpntCount = fStringVals.Length;
 
                 //if this is the first file, then create idXVal array for contents of files
-                if (f == 0) idxVal = new double[fileCount, idxCount];
+                if (f == 0)
+                {
+                    dpntArray = new double[fileCount, dpntCount];
+                    cplxArray = new Complex[fileCount, dpntCount];
+                    fpntArray = new Complex[fileCount, dpntCount];
+                }
+
                 //copy the contents into the idxVals
                 for (int i = 0; i < fStringVals.Length; i++)
                 {
-                    idxVal[f, i] = Convert.ToDouble(fStringVals[i].Split(spc, StringSplitOptions.RemoveEmptyEntries)[1]);
+                    dpntArray[f, i] = Convert.ToDouble(fStringVals[i].Split(spc, StringSplitOptions.RemoveEmptyEntries)[1]);
+                    cplxArray[f, i] = new Complex(dpntArray[f, i], 0);
                 }
-            }
-
-            //Compute average (mean) for each position
-            double oddAvg;
-            double evenAvg;
-            int evenCnt;
-            int oddCnt;
-            string clipOut = null;
-
-            double[] avgVals = new double[idxCount];  //average per even sample
-            double[] noiseVals = new double[idxCount];  //average per odd sample
-
-            for (int idx = 0; idx < idxCount; idx++)
-            {
-                evenAvg = 0;
-                evenCnt = 0;
-                oddAvg = 0;
-                oddCnt = 0;
-                for (int f = 0; f < fileCount; f += 2)
-                {
-                    evenAvg += idxVal[f, idx];
-                    evenCnt++;
-                }
-                for (int f = 1; f < fileCount; f += 2)
-                {
-                    oddAvg += idxVal[f, idx];
-                    oddCnt++;
-                }
-                evenAvg /= evenCnt;
-                oddAvg /= oddCnt;
-
-                avgVals[idx] = (evenAvg + oddAvg) / 2;
-                noiseVals[idx] = Math.Abs(oddAvg - evenAvg) / 2;
-                string indStr = idx.ToString().PadLeft(4);
-                string valStr = ((int)Math.Round(avgVals[idx])).ToString().PadLeft(5);
-                clipOut += indStr + valStr + "\r\n";
             }
 
             //Let's graph this
-            PECchart.ChartAreas[0].AxisX.Minimum = 0;
-            PECchart.Series.Clear();
+            PECCurve.ChartAreas[0].AxisX.Minimum = 0;
+            PECCurve.Series.Clear();
+            PECFrequency.ChartAreas[0].AxisX.Minimum = 0;
+            PECFrequency.Series.Clear();
+
             for (int f = 0; f < fileCount; f++)
             {
-                Series iSeries = new Series("PEC " + f.ToString());
+                //Series iSeries = new Series("PEC " + f.ToString());
+                char[] splitter = { '.' };
+                string[] shortNames = pecfiles[f].Split(splitter);
+                string fileShortName = shortNames[shortNames.Length - 2];
+                Series iSeries = new Series("Curve " + fileShortName);
                 iSeries.XValueType = ChartValueType.Int32;
                 iSeries.ChartType = SeriesChartType.FastLine;
                 iSeries.MarkerStyle = MarkerStyle.Circle;
-                iSeries.Color = Color.LightBlue;
-                for (int idx = 0; idx < idxCount; idx++)
+                iSeries.BorderWidth = logCurveBorderWidth;
+                iSeries.Color = logCurveColor;
+                for (int idx = 0; idx < dpntCount; idx++)
                 {
-                    iSeries.Points.AddXY(idx, Math.Round(idxVal[f, idx]));
+                    iSeries.Points.AddXY(idx, Math.Round(dpntArray[f, idx]));
                 }
-                PECchart.Series.Add(iSeries);
+                Complex[] fdx = new Complex[dpntCount];
+                for (int c = 0; c < dpntCount; c++)
+                {
+                    fdx[c] = dpntArray[f, c];
+                }
+                // Compute DFT for each Series
+                FourierTransform.DFT(fdx, FourierTransform.Direction.Forward);
+                for (int c = 0; c < dpntCount; c++)
+                {
+                    fpntArray[f, c] = fdx[c];
+                }
+
+                Series fSeries = new Series("PECF " + fileShortName);
+                fSeries.XValueType = ChartValueType.Double;
+                fSeries.ChartType = SeriesChartType.Point;
+                fSeries.MarkerStyle = MarkerStyle.Diamond;
+                fSeries.BorderWidth = logFreqBorderWidth;
+                fSeries.Color = logFreqColor;
+
+                for (int idx = 0; idx <= maxPeriod; idx++)
+                {
+                    //int iFreq = idx + 1;
+                    fSeries.Points.AddXY(idx, fdx[idx].Magnitude);
+                }
+
+                PECCurve.Series.Add(iSeries);
+                PECFrequency.Series.Add(fSeries);
             }
 
             //graph the average and noise, if more than one PEC curve has been selected
-            if (fileCount > 1)
+            int fileMap = (int)Math.Pow(2, fileCount) - 1;
+            SignalAverage(fileMap);
+
+            Series aSeries = new Series("Average");
+            aSeries.XValueType = ChartValueType.Int32;
+            aSeries.ChartType = SeriesChartType.Line;
+            aSeries.MarkerStyle = MarkerStyle.Circle;
+            aSeries.BorderWidth = avgCurveBorderWidth;
+            aSeries.Color = avgCurveColor;
+
+            for (int idx = 0; idx < avgVals.Length; idx++)
             {
-                Series aSeries = new Series("Average");
-                aSeries.XValueType = ChartValueType.Int32;
-                aSeries.ChartType = SeriesChartType.FastLine;
-                aSeries.MarkerStyle = MarkerStyle.Circle;
-                aSeries.Color = Color.Green;
-                for (int idx = 0; idx < avgVals.Length; idx++)
-                {
-                    aSeries.Points.AddXY(idx, (int)avgVals[idx]);
-                }
-                PECchart.Series.Add(aSeries);
-
-                Series nSeries = new Series("Est Noise");
-                nSeries.XValueType = ChartValueType.Int32;
-                nSeries.ChartType = SeriesChartType.FastLine;
-                nSeries.MarkerStyle = MarkerStyle.Circle;
-                nSeries.Color = Color.Red;
-                for (int idx = 0; idx < noiseVals.Length; idx++)
-                {
-                    nSeries.Points.AddXY(idx, noiseVals[idx]);
-                }
-                PECchart.Series.Add(nSeries);
-
-                //Compute estimated noise via odd/even averages
-                double noiseEstimate = 0;
-                for (int idx = 0; idx < idxCount; idx += 2) { noiseEstimate += noiseVals[idx]; }
-                noiseEstimate /= (idxCount / 2);
-                noiseTextBox.Text = noiseEstimate.ToString("0.000");
-
+                aSeries.Points.AddXY(idx, (int)avgVals[idx]);
             }
+            PECCurve.Series.Add(aSeries);
+
+            Series nSeries = new Series("Est Noise");
+            nSeries.XValueType = ChartValueType.Int32;
+            nSeries.ChartType = SeriesChartType.FastLine;
+            nSeries.MarkerStyle = MarkerStyle.Circle;
+            aSeries.BorderWidth = noiseBorderWidth;
+            nSeries.Color = noiseColor;
+
+            PECCurve.ChartAreas[0].RecalculateAxesScale();
+            PECFrequency.ChartAreas[0].RecalculateAxesScale();
+
+            for (int idx = 0; idx < noiseVals.Length; idx++)
+            {
+                nSeries.Points.AddXY(idx, noiseVals[idx]);
+            }
+            PECCurve.Series.Add(nSeries);
+
+            //Compute estimated noise via odd/even averages
+
+            noiseTextBox.Text = NoiseEstimate().ToString("0.000");
+
+            //Compute RMS of frequencies
+            Series frmsSeries = new Series("Mean Frequency");
+            frmsSeries.XValueType = ChartValueType.Int32;
+             frmsSeries.IsValueShownAsLabel = true;
+           frmsSeries.ChartType = SeriesChartType.Spline;
+            frmsSeries.MarkerStyle = MarkerStyle.Circle;
+            frmsSeries.LabelFormat = "G2";
+            frmsSeries.Color = medianFreqColor;
+            frmsSeries.BorderWidth = medianFreqBorderWidth;
+
+            Complex[] meanFreqs = RMS(fpntArray, fileCount, dpntCount);
+            for (int c = 0; c <= maxPeriod; c++)
+            {
+                frmsSeries.Points.AddXY(c, meanFreqs[c].Magnitude);
+                //frmsSeries.Points[c].Label = meanFreqs[c].Magnitude.ToString("0.00");
+            }
+            PECFrequency.Series.Add(frmsSeries);
+
+            int filePick = BestBehavior(fpntArray, meanFreqs);
+            char[] fsplitter = { '.','\\','/' };
+            string[] fshortNames = pecfiles[filePick].Split(fsplitter);
+            string ffileShortName = fshortNames[fshortNames.Length - 2];
+
+            BestLogBox.Text = ffileShortName;
+
+            noiseTextBox.Text = NoiseEstimate().ToString("0.00");
             //Copy the result to the clipboard
             Clipboard.SetText(clipOut);
             return;
         }
 
+        public static Complex[] RMS(Complex[,] dataArray, int dataSets, int dataPnts)
+        {
+            Complex[] meanVctr = new Complex[dataPnts];
+            //Compute average 
+            for (int pnt = 0; pnt < dataPnts; pnt++)
+            {
+                Complex mean = 0;
+                for (int set = 0; set < dataSets; set++)
+                {
+                    mean = Complex.Add(Complex.Pow(dataArray[set, pnt], 2), mean);
+                }
+                meanVctr[pnt] = Complex.Sqrt(Complex.Divide(mean, dataSets));
+            }
+            return meanVctr;
+        }
+
+        private int BestBehavior(Complex[,] fpnts, Complex[] mpnts)
+        {
+            //figure out which combination of curve data produces the best match with the mean
+            //For each set of fpnts, compute a sum of difference between freq and mean
+            double[] fdif = new double[fileCount];
+            for (int df = 0; df < fileCount; df++)
+            {
+                fdif[df] = 0;
+                for (int dpt = 0; dpt < mpnts.Length / 2; dpt++)
+                {
+                    fdif[df] += Math.Abs(fpnts[df, dpt].Magnitude - mpnts[dpt].Magnitude);
+                }
+                fdif[df] /= mpnts.Length / 2;
+            }
+            //Find the smallest value
+            double min = fdif[0];
+            int minIndex = 0;
+            for (int i = 1; i < fileCount; i++)
+            {
+                if (fdif[i] < min)
+                {
+                    minIndex = i;
+                    min = fdif[i];
+                }
+            }
+            return minIndex;
+        }
+
+
+        private void SignalAverage(int fileMap)
+        {
+            //Compute average (mean) for each position
+            double oddAvg;
+            double evenAvg;
+            int evenCnt;
+            int oddCnt;
+
+            avgVals = new double[dpntCount];  //average per even sample
+            noiseVals = new double[dpntCount];  //average per odd sample
+
+            for (int idx = 0; idx < dpntCount; idx++)
+            {
+                evenAvg = 0;
+                evenCnt = 0;
+                oddAvg = 0;
+                oddCnt = 0;
+
+                for (int f = 0; f < fileCount; f += 2)
+                {
+                    if ((fileMap & (f + 1)) != 0)
+                    {
+                        evenAvg += dpntArray[f, idx];
+                        evenCnt++;
+                    }
+                }
+                for (int f = 1; f < fileCount; f += 2)
+                {
+                    if ((fileMap & (f + 1)) != 0)
+                    {
+                        oddAvg += dpntArray[f, idx];
+                        oddCnt++;
+                    }
+                }
+                evenAvg /= evenCnt;
+                oddAvg /= oddCnt;
+
+                if (fileCount > 1)
+                {
+                    avgVals[idx] = Math.Round((evenAvg + oddAvg) / 2.0);
+                    noiseVals[idx] = Math.Abs(oddAvg - evenAvg) / 2.0;
+                }
+                else
+                {
+                    avgVals[idx] = dpntArray[0, idx];
+                    noiseVals[idx] = 0;
+                }
+
+                string indStr = idx.ToString().PadLeft(4);
+                string valStr = ((int)Math.Round(avgVals[idx])).ToString().PadLeft(5);
+                clipOut += indStr + valStr + "\r\n";
+
+            }
+        }
+
+        private double NoiseEstimate()
+        {
+            //Compute estimated noise via odd/even averages
+            double noiseEstimate = 0;
+            for (int idx = 0; idx < dpntCount; idx += 2) { noiseEstimate += noiseVals[idx]; }
+            noiseEstimate /= (dpntCount / 2);
+            return Math.Abs(noiseEstimate);
+        }
+
+        private double SignalEstimate()
+        {
+            //Compute estimated noise via odd/even averages
+            double signalEstimate = 0;
+            for (int idx = 1; idx < dpntCount; idx += 2) { signalEstimate += avgVals[idx]; }
+            signalEstimate /= (dpntCount / 2);
+            return Math.Abs(signalEstimate);
+        }
 
     }
+
 }
+
+
 
